e027976eb3c772949782c4e5e9ee2b9001fb9db9
Sven Schreiber <sven-schreiber@outlook.de>
2020-10-07 22:49:30 +0200

    moved sql stuff into seperate file

--- bot/herbot.py
+++ bot/herbot.py
@@ -1,26 +1,24 @@
 import discord
 import os
 import datetime
-import mysql.connector
 from datetime import datetime
 from random import randint
 import shlex
+from .sql import SQL
 
 class Herbot(discord.Client):
     
     def __init__(self):
         super().__init__()
-        self.__db = self.__setup_db()
-        self.__cursor = self.__db.cursor(buffered=True)
+        self.__sql = SQL(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), os.getenv("MYSQL_DATABASE"))
         
 
     async def on_ready(self):
         print("[HERBOT] ist am start yo.")
 
     async def on_message(self, message):
-        if not self.__db.is_connected():
-            self.__db.reconnect()
-            self.__cursor.execute("USE herbot")
+        if not self.__sql.is_connected():
+            self.__sql.reconnect()
 
         if message.author == self.user:
             return
@@ -33,64 +31,60 @@ class Herbot(discord.Client):
         
         if highest_role < role_minimum: return
 
+        display_name = message.author.display_name
+
         # create user if not exist
-        if not self.__user_exists(message.author.display_name):
-            self.__cursor.execute(f"INSERT INTO users (display_name) VALUES ('{message.author.display_name}')")
-            self.__db.commit()
+        if not self.__sql.user_exists(display_name):
+            self.__sql.add_user(display_name)
         
         msg = shlex.split(message.content)
         command = msg[0]
         args = msg[1:]
-        display_name = message.author.display_name
 
         # check for text command and get it's message if exists
-        self.__cursor.execute(f"SELECT * FROM commands WHERE command ='{command}'")
-        result = self.__cursor.fetchall()
-        if result and result[0][0] == command:
-            await message.channel.send(result[0][1])
+        if self.__sql.text_command_exists(command):
+            text = self.__sql.get_value_where("text", "commands", ("command", command))
+            await message.channel.send(text)
 
         # check for other commands
-        self.__check_next_date()
+        self.__sql.check_next_date()
 
         if command == "!schwanz":
             if len(args) == 0:
-                if not self.__already_typed_schwanz(display_name, "schwanz"):
+                if not self.__sql.schwanz_on_cooldown(display_name, "schwanz"):
                     laenge = randint(0, 100)
                     await message.channel.send(f"{display_name} hat einen {laenge}cm Schwanz.")
-                    self.__cursor.execute(f"UPDATE users SET schwanz = {laenge} WHERE display_name = '{display_name}'")
-                    self.__db.commit()
-                    self.__process_highscore(display_name)
+                    self.__sql.update_schwanz_laenge(display_name, "schwanz", laenge)
+                    self.__sql.process_highscore(display_name)
                 else:
-                    laenge = self.__get_user_value(display_name, "schwanz")
+                    laenge = self.__sql.get_schwanz_laenge(display_name, "schwanz")
                     await message.channel.send(f"{display_name} hatte heute einen {laenge}cm Schwanz.")
         elif command == "!yarak":
             if len(args) == 0:
-                if not self.__already_typed_schwanz(display_name, "yarak"):
+                if not self.__sql.schwanz_on_cooldown(display_name, "yarak"):
                     laenge = randint(0, 100)
                     await message.channel.send(f"{display_name} hat einen {laenge}cm Yarak.")
-                    self.__cursor.execute(f"UPDATE users SET yarak = {laenge} WHERE display_name = '{display_name}'")
-                    self.__db.commit()
-                    self.__process_highscore(display_name)
+                    self.__sql.update_schwanz_laenge(display_name, "yarak", laenge)
+                    self.__sql.process_highscore(display_name)
                 else:
-                    laenge = self.__get_user_value(display_name, "yarak")
+                    laenge = self.__sql.get_schwanz_laenge(display_name, "yarak")
                     await message.channel.send(f"{display_name} hatte heute einen {laenge}cm Yarak.")
         elif command == "!subschwanz":
             if len(args) == 0:
-                if not self.__already_typed_schwanz(display_name, "subschwanz"):
+                if not self.__sql.schwanz_on_cooldown(display_name, "subschwanz"):
                     laenge = randint(0, 200)
                     await message.channel.send(f"{display_name} hat einen {laenge}cm Subschwanz.")
-                    self.__cursor.execute(f"UPDATE users SET subschwanz = {laenge} WHERE display_name = '{display_name}'")
-                    self.__db.commit()
-                    self.__process_highscore(display_name)
+                    self.__sql.update_schwanz_laenge(display_name, "subschwanz", laenge)
+                    self.__sql.process_highscore(display_name)
                 else:
-                    laenge = self.__get_user_value(display_name, "subschwanz")
+                    laenge = self.__sql.get_schwanz_laenge(display_name, "subschwanz")
                     await message.channel.send(f"{display_name} hatte heute einen {laenge}cm Subschwanz.")
         elif command == "!schwanzinfo":
             if len(args) == 0:
                 embed = self.__get_schwanzinfo_embed(display_name)
                 await message.channel.send(embed=embed)
             elif len(args) == 1:
-                if self.__user_exists(args[0]):
+                if self.__sql.user_exists(args[0]):
                     embed = self.__get_schwanzinfo_embed(args[0])
                     await message.channel.send(embed=embed)
                 else:
@@ -100,11 +94,11 @@ class Herbot(discord.Client):
                 ranks = ""
                 users = ""
                 laengen = ""
-                bl = self.__get_bestenliste()[:10]
+                bl = self.__sql.get_ordered_highscores()[:10]
                 for i in range(len(bl)):
                     ranks += f"#{i+1}\n"
                     users += f"{bl[i][0]}\n"
-                    laengen += f"{bl[i][4]}cm\n"
+                    laengen += f"{bl[i][1]}cm\n"
                 
                 embed = discord.Embed(title=f"Bestenliste (Top 10)")
                 embed.add_field(name="Rank", value=ranks)
@@ -113,25 +107,22 @@ class Herbot(discord.Client):
                 await message.channel.send(embed=embed)
         elif command == "!addcom":
             if len(args) == 2:
-                if not self.__is_text_command(args[0]):
-                    self.__cursor.execute("INSERT INTO commands (command, text) VALUES (%s, %s)", (args[0], args[1]))
-                    self.__db.commit()
+                if not self.__sql.text_command_exists(args[0]):
+                    self.__sql.add_text_command(args[0], args[1])
                     await message.channel.send(f"Der Command \"{args[0]}\" wurde hinzugefügt.")
                 else:
                     await message.channel.send(f"Den Command \"{args[0]}\" gibt es schon.")
         elif command == "!delcom":
             if len(args) == 1:
-                if self.__is_text_command(args[0]):
-                    self.__cursor.execute(f"DELETE FROM commands WHERE command = '{args[0]}'")
-                    self.__db.commit()
+                if self.__sql.text_command_exists(args[0]):
+                    self.__sql.delete_text_command(args[0])
                     await message.channel.send(f"Der Command \"{args[0]}\" wurde gelöscht.")
                 else:
                     await message.channel.send(f"Den Command \"{args[0]}\" gibt es nicht.")
         elif command == "!editcom":
             if len(args) == 2:
-                if self.__is_text_command(args[0]):
-                    self.__cursor.execute(f"UPDATE commands SET text = '{args[1]}' WHERE command = '{args[0]}'")
-                    self.__db.commit()
+                if self.__sql.text_command_exists(args[0]):
+                    self.__sql.edit_text_command(args[0], args[1])
                     await message.channel.send(f"Der Command \"{args[0]}\" wurde bearbeitet.")
                 else:
                     await message.channel.send(f"Den Command \"{args[0]}\" gibt es nicht.")
@@ -139,93 +130,28 @@ class Herbot(discord.Client):
     def __get_schwanzinfo_embed(self, display_name):
         embed = discord.Embed(title=f"{display_name} Schwanzinfo")
         embed.add_field(name="Typ", value="Schwanz\nYarak\nSubschwanz\nInsgesamt")
-        schwaenze = [self.__get_user_value(display_name, "schwanz"), self.__get_user_value(display_name, "yarak"), self.__get_user_value(display_name, "subschwanz")]
+        schwaenze = [
+            self.__sql.get_schwanz_laenge(display_name, "schwanz"),
+            self.__sql.get_schwanz_laenge(display_name, "yarak"),
+            self.__sql.get_schwanz_laenge(display_name, "subschwanz")
+        ]
         insgesamt = 0
-        for i in range(len(schwaenze)):
-            if schwaenze[i] == None:
-                schwaenze[i] == "- "
+        for n, i in enumerate(schwaenze):
+            if i == None:
+                schwaenze[n] == "- "
             else:
-                insgesamt += schwaenze[i]
-        bl = self.__get_bestenliste()
+                insgesamt += schwaenze[n]
+        bl = self.__sql.get_ordered_highscores()
         highscore = None
         rank = None
         for i in range(len(bl)):
             if bl[i][0] == display_name:
-                highscore = bl[i][4]
+                highscore = bl[i][1]
                 rank = i+1
-        highscore = self.__get_user_value(display_name, "highscore")
+        highscore = self.__sql.get_value_where("highscore", "users", ("display_name", display_name))
         if highscore == None: highscore = "- "
         if rank == None: rank = "-"
         
         embed.add_field(name="Länge", value=f"{schwaenze[0]}cm\n{schwaenze[1]}cm\n{schwaenze[2]}cm\n{insgesamt}cm")
         embed.add_field(name="Highscore", value=f"{highscore}cm (#{rank})", inline=False)
         return embed
-
-    def __is_text_command(self, command):
-        self.__cursor.execute(f"SELECT * FROM commands WHERE command ='{command}'")
-        result = self.__cursor.fetchall()
-        if result and result[0][0] == command: return True
-        else: return False
-
-    def __check_next_date(self):
-        self.__cursor.execute(f"SELECT * FROM settings WHERE name = 'next_date'")
-        result = self.__cursor.fetchall()
-        if result:
-            next_date = datetime.strptime(result[0][1], '%Y-%m-%d').date()
-            if datetime.today().date() > next_date:
-                self.__cursor.execute("UPDATE users SET schwanz = NULL, yarak = NULL, subschwanz = NULL")
-                self.__cursor.execute(f"UPDATE settings SET val = '{datetime.today().date()}' WHERE name = 'next_date'")
-                self.__db.commit()
-        else:
-            next_date = str(datetime.today().date())
-            self.__cursor.execute("INSERT INTO settings (name, val) VALUES (%s, %s)", ("next_date", next_date))
-            self.__db.commit()
-
-    def __already_typed_schwanz(self, display_name, schwanz_type):
-        self.__cursor.execute(f"SELECT {schwanz_type} FROM users WHERE display_name = '{display_name}'")
-        result = self.__cursor.fetchone()
-        if result[0] != None:
-            return True
-        else:
-            return False
-
-    def __get_bestenliste(self):
-        self.__cursor.execute("SELECT * FROM users ORDER BY highscore DESC")
-        result = self.__cursor.fetchall()
-        for x in result:
-            if x[4] == None:
-                result.remove(x)
-        return result
-
-    def __get_user_value(self, display_name, column):
-        self.__cursor.execute(f"SELECT {column} FROM users WHERE display_name = '{display_name}'")
-        result = self.__cursor.fetchone()
-        return result[0]
-
-    def __user_exists(self, display_name):
-        self.__cursor.execute(f"SELECT * FROM users WHERE display_name = '{display_name}'")
-        result = self.__cursor.fetchone()
-        if result: return True
-        else: return False
-
-    def __process_highscore(self, display_name):
-        self.__cursor.execute(f"SELECT * FROM users WHERE display_name = '{display_name}'")
-        result = self.__cursor.fetchone()
-        if result[1] != None and result[2] != None and result[3] != None:
-            sum = result[1] + result[2] + result[3]
-            if result[4] != None:
-                if result[4] < sum:
-                    self.__cursor.execute(f"UPDATE users SET highscore = {sum} WHERE display_name = '{display_name}'")
-                    self.__db.commit()
-            else:
-                self.__cursor.execute(f"UPDATE users SET highscore = {sum} WHERE display_name = '{display_name}'")
-                self.__db.commit()
-
-    def __setup_db(self):
-        db = mysql.connector.connect(host=os.getenv('MYSQL_HOST'), user=os.getenv('MYSQL_USER'), password=os.getenv('MYSQL_PASSWORD'))
-        db.cursor().execute("CREATE DATABASE IF NOT EXISTS herbot")
-        db.cursor().execute("USE herbot")
-        db.cursor().execute("CREATE TABLE IF NOT EXISTS commands (command VARCHAR(32) PRIMARY KEY, text VARCHAR(1024))")
-        db.cursor().execute("CREATE TABLE IF NOT EXISTS users (display_name VARCHAR(64) PRIMARY KEY, schwanz TINYINT(3) UNSIGNED DEFAULT NULL, yarak TINYINT(3) UNSIGNED DEFAULT NULL, subschwanz TINYINT(3) UNSIGNED DEFAULT NULL, highscore SMALLINT(3) UNSIGNED DEFAULT NULL)")
-        db.cursor().execute("CREATE TABLE IF NOT EXISTS settings (name VARCHAR(32) PRIMARY KEY, val VARCHAR(512))")
-        return db
---
+++ bot/sql.py
@@ -0,0 +1,90 @@
+from mysql import connector
+from datetime import datetime
+
+class SQL():
+    def __init__(self, host, user, password, database):
+        self.__db = connector.connect(host=host, user=user, passwd=password, database=database)
+        self.__cursor = self.__db.cursor(buffered=True)
+        self.__create_tables()
+        
+
+    def __create_tables(self):
+        self.__cursor.execute("CREATE TABLE IF NOT EXISTS commands (command VARCHAR(32) PRIMARY KEY, text VARCHAR(1024))")
+        self.__cursor.execute("CREATE TABLE IF NOT EXISTS users (display_name VARCHAR(64) PRIMARY KEY, schwanz TINYINT(3) UNSIGNED DEFAULT NULL, yarak TINYINT(3) UNSIGNED DEFAULT NULL, subschwanz TINYINT(3) UNSIGNED DEFAULT NULL, highscore SMALLINT(3) UNSIGNED DEFAULT NULL)")
+        self.__cursor.execute("CREATE TABLE IF NOT EXISTS settings (name VARCHAR(32) PRIMARY KEY, val VARCHAR(512))")
+
+    def reconnect(self):
+        self.__db.reconnect()
+        self.__cursor.execute("USE herbot")
+
+    def is_connected(self):
+        return self.__db.is_connected()
+
+    def execute(self, statement, data):
+        self.__cursor.execute(statement, data)
+        self.__db.commit()
+
+    def query(self, statement, data):
+        self.__cursor.execute(statement, data)
+        return self.__cursor.fetchall()
+
+    def text_command_exists(self, command):
+        result = self.query("SELECT command FROM commands WHERE command = %s", (command,))
+        if result: return True
+        else: return False
+
+    def check_next_date(self):
+        result = self.query("SELECT val FROM settings WHERE name = %s", ('next_date',))
+        if result:
+            next_date = datetime.strptime(result[0][0], '%Y-%m-%d').date()
+            if datetime.today().date() > next_date:
+                self.execute("UPDATE users SET schwanz = %s, yarak = %s, subschwanz = %s", (None, None, None))
+                self.execute("UPDATE settings SET val = %s WHERE name = %s", (datetime.today().date(), 'next_date'))
+        else:
+            self.execute("INSERT INTO settings (name, val) VALUES (%s, %s)", ('next_date', datetime.today().date()))
+ 
+    def schwanz_on_cooldown(self, display_name, schwanz_type):
+        result = self.query("SELECT {} from users WHERE display_name = %s".format(schwanz_type), (display_name,))
+        if result == None: return True
+        else: return False
+
+    def get_ordered_highscores(self):
+        return self.query("SELECT display_name, highscore from users WHERE highscore IS NOT NULL ORDER BY highscore DESC", ())
+
+    def get_value_where(self, column, table, where):
+        return self.query("SELECT {} FROM {} WHERE {} = %s".format(column, table, where[0]), (where[1],))[0][0]
+
+    def update_value_where(self, table, column, value, where):
+        self.execute("UPDATE {} SET {} = %s WHERE {} = %s".format(table, column, where[0]), (value, where[1]))
+
+    def user_exists(self, display_name):
+        result = self.query("SELECT display_name FROM users WHERE display_name = %s", (display_name,))
+        if result: return True
+        else: return False
+
+    def process_highscore(self, display_name):
+        result = self.query("SELECT schwanz, yarak, subschwanz, highscore FROM users WHERE display_name = %s", (display_name,))
+        print(result)
+        highscore = result[0][3]
+        if None not in result[0][0:3]:
+            schwanz_summe = sum(result[0][0:3])
+            if highscore is None or highscore < schwanz_summe:
+                self.execute("UPDATE users SET highscore = %s WHERE display_name = %s", (schwanz_summe, display_name))
+
+    def add_user(self, display_name):
+        self.execute("INSERT INTO users (display_name) VALUES (%s)", (display_name,))
+
+    def update_schwanz_laenge(self, display_name, schwanz_type, laenge):
+        self.update_value_where("users", schwanz_type, laenge, ("display_name", display_name))
+
+    def get_schwanz_laenge(self, display_name, schwanz_type):
+        return self.get_value_where(schwanz_type, "users", ("display_name", display_name))
+
+    def add_text_command(self, command, text):
+        self.execute("INSERT INTO commands (command, text) VALUES (%s, %s)", (command, text))
+
+    def delete_text_command(self, command):
+        self.execute("DELETE FROM commands WHERE command = %s", (command,))
+
+    def edit_text_command(self, command, text):
+        self.update_value_where("commands", "text", text, ("command", command))
