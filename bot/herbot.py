2a3b41d9b834e89de0cf8b5e9516401de72a3be7
wiresven <sven-schreiber@outlook.de>
2021-03-29 12:53:55 +0200

    Man kann jetzt seinen Nickname ändern und behält seine Stats PogChamp

--- bot/herbot.py
+++ bot/herbot.py
@@ -11,109 +11,108 @@ class Herbot(discord.Client):
     
     def __init__(self):
         super().__init__(intents=discord.Intents.all())
-        self.__sql = SQL(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), os.getenv("MYSQL_DATABASE"))
+        self.sql = SQL(os.getenv("MYSQL_HOST"), os.getenv("MYSQL_USER"), os.getenv("MYSQL_PASSWORD"), os.getenv("MYSQL_DATABASE"))
         self.enabled_members = [(408639338910449664, 505060092811411493)]
         self.ignored_voice_channels = [408730091682791425]
+        self.embed_color = discord.Color.gold()
         
     async def on_ready(self):
         self.count_time.start()
         print("[HERBOT] ist am start yo.")
 
     async def on_message(self, message):
-        if not self.__sql.is_connected():
-            self.__sql.reconnect()
+        if not self.sql.is_connected():
+            self.sql.reconnect()
 
-        if message.author == self.user:
+        user = message.author
+
+        if user == self.user:
             return
 
         role_minimum = 6
         highest_role = 0
-        for role in message.author.roles:
+        for role in user.roles:
             if highest_role < role.position:
                 highest_role = role.position
         
         if highest_role < role_minimum: return
 
-        display_name = message.author.display_name
-        color = discord.Color.gold()
-
-        # create user if not exist
-        if not self.__sql.user_exists(display_name):
-            self.__sql.add_user(display_name)
+        # create user if it does not exist or update display_name if changed
+        self.sql.check_user(user)
         
         msg = shlex.split(message.content)
         command = msg[0].lower()
         args = msg[1:]
 
         # check for text command and get it's message if exists
-        if self.__sql.text_command_exists(command):
-            text = self.__sql.get_value_where("text", "commands", ("command", command))
+        if self.sql.text_command_exists(command):
+            text = self.sql.get_value_where("text", "commands", ("command", command))
             await message.channel.send(text)
 
         # check for other commands
-        self.__sql.check_next_date()
+        self.sql.check_next_date()
 
         if command == "!schwanz":
             if len(args) == 0:
-                if not self.__sql.schwanz_on_cooldown(display_name, "schwanz"):
+                if not self.sql.schwanz_on_cooldown(user, "schwanz"):
                     laenge = randint(0, 100)
-                    await message.channel.send(f"{display_name} hat einen {laenge}cm Schwanz.")
-                    self.__sql.update_schwanz_laenge(display_name, "schwanz", laenge)
-                    self.__sql.process_highscore(display_name)
+                    await message.channel.send(f"{user.display_name} hat einen {laenge}cm Schwanz.")
+                    self.sql.update_schwanz_laenge(user, "schwanz", laenge)
+                    self.sql.process_highscore(user)
                 else:
-                    laenge = self.__sql.get_schwanz_laenge(display_name, "schwanz")
-                    await message.channel.send(f"{display_name} hatte heute einen {laenge}cm Schwanz.")
+                    laenge = self.sql.get_schwanz_laenge(user, "schwanz")
+                    await message.channel.send(f"{user.display_name} hatte heute einen {laenge}cm Schwanz.")
         elif command == "!yarak":
             if len(args) == 0:
-                if not self.__sql.schwanz_on_cooldown(display_name, "yarak"):
+                if not self.sql.schwanz_on_cooldown(user, "yarak"):
                     laenge = randint(0, 100)
-                    await message.channel.send(f"{display_name} hat einen {laenge}cm Yarak.")
-                    self.__sql.update_schwanz_laenge(display_name, "yarak", laenge)
-                    self.__sql.process_highscore(display_name)
+                    await message.channel.send(f"{user.display_name} hat einen {laenge}cm Yarak.")
+                    self.sql.update_schwanz_laenge(user, "yarak", laenge)
+                    self.sql.process_highscore(user)
                 else:
-                    laenge = self.__sql.get_schwanz_laenge(display_name, "yarak")
-                    await message.channel.send(f"{display_name} hatte heute einen {laenge}cm Yarak.")
+                    laenge = self.__sql.get_schwanz_laenge(user, "yarak")
+                    await message.channel.send(f"{user.display_name} hatte heute einen {laenge}cm Yarak.")
         elif command == "!subschwanz":
             if len(args) == 0:
-                if not self.__sql.schwanz_on_cooldown(display_name, "subschwanz"):
+                if not self.sql.schwanz_on_cooldown(user, "subschwanz"):
                     laenge = randint(0, 200)
-                    await message.channel.send(f"{display_name} hat einen {laenge}cm Subschwanz.")
-                    self.__sql.update_schwanz_laenge(display_name, "subschwanz", laenge)
-                    self.__sql.process_highscore(display_name)
+                    await message.channel.send(f"{user.display_name} hat einen {laenge}cm Subschwanz.")
+                    self.sql.update_schwanz_laenge(user, "subschwanz", laenge)
+                    self.sql.process_highscore(user)
                 else:
-                    laenge = self.__sql.get_schwanz_laenge(display_name, "subschwanz")
-                    await message.channel.send(f"{display_name} hatte heute einen {laenge}cm Subschwanz.")
+                    laenge = self.sql.get_schwanz_laenge(user, "subschwanz")
+                    await message.channel.send(f"{user.display_name} hatte heute einen {laenge}cm Subschwanz.")
         elif command == "!stats":
             if len(args) == 0:
-                embed = self.__get_stats_embed(message.author, color)
+                embed = self.get_stats_embed(user, self.embed_color)
                 await message.channel.send(embed=embed)
             elif len(args) == 1:
-                if self.__sql.user_exists(args[0]):
-                    for user in message.channel.members:
-                        if user.display_name.lower() == args[0].lower():
-                            embed = self.__get_stats_embed(user, color)
+                if self.sql.user_exists(args[0]):
+                    for member in message.channel.members:
+                        if member.display_name.lower() == args[0].lower():
+                            embed = self.get_stats_embed(member, self.embed_color)
                             await message.channel.send(embed=embed)
                 else:
                     await message.channel.send(f"Es gibt den User \"{args[0]}\" nicht.")
         elif command == "!187":
-            count = self.__sql.get_value_where("187_count", "users", ("display_name", display_name))
-            await message.channel.send(f"{display_name} hat {count} mal 187 gehabt. :one: :eight: :seven:")
+            count = self.sql.get_value_where("187_count", "users", ("id", user.id))
+            await message.channel.send(f"{user.display_name} hat {count} mal 187 gehabt. :one: :eight: :seven:")
         elif command == "!69":
-            count = self.__sql.get_value_where("69_count", "users", ("display_name", display_name))
-            await message.channel.send(f"{display_name} hat {count} mal 69 gehabt. nice. :joy:")
+            count = self.sql.get_value_where("69_count", "users", ("id", user.id))
+            await message.channel.send(f"{user.display_name} hat {count} mal 69 gehabt. nice. :joy:")
         elif command == "!88":
-            count = self.__sql.get_value_where("88_count", "users", ("display_name", display_name))
-            await message.channel.send(f"{display_name} hat {count} mal 88 gehabt. :sunglasses:")
+            count = self.sql.get_value_where("88_count", "users", ("id", user.id))
+            await message.channel.send(f"{user.display_name} hat {count} mal 88 gehabt. :sunglasses:")
         elif command == "!1337":
-            count = self.__sql.get_value_where("1337_count", "users", ("display_name", display_name))
+            count = self.sql.get_value_where("1337_count", "users", ("id", user.id))
             time = datetime.now().time()
             if time.hour == 13 and time.minute == 37:
-                already_did_today = self.__sql.get_value_where("1337_today", "users", ("display_name", display_name))
+                already_did_today = self.sql.get_value_where("1337_today", "users", ("id", user.id))
                 if not already_did_today:
-                    await message.channel.send(f"Juhu, es ist 13:37 Uhr! Du warst jetzt {count + 1} mal um 13:37 Uhr da.")
-                    self.__sql.add_1337_count(display_name, 1)
+                    await message.channel.send(f"Juhu, es ist 13:37 Uhr! {user.display_name} war jetzt {count + 1} mal um 13:37 Uhr da.")
+                    self.sql.add_1337_count(user, 1)
                     return
-            await message.channel.send(f"Du warst {count} mal um 13:37 Uhr da.")
+            await message.channel.send(f"{user.display_name} war {count} mal um 13:37 Uhr da.")
             
                 
         elif command == "!bestenliste":
@@ -121,14 +120,14 @@ class Herbot(discord.Client):
                 ranks = ""
                 users = ""
                 laengen = ""
-                bl = self.__sql.get_ordered_highscores()[:10]
+                bl = self.sql.get_ordered_highscores()[:10]
                 for i in range(len(bl)):
                     ranks += f"#{i+1}\n"
-                    users += f"{bl[i][0]}\n"
-                    laengen += f"{bl[i][1]}cm\n"
+                    users += f"{bl[i][1]}\n"
+                    laengen += f"{bl[i][2]}cm\n"
 
                 if not bl: await message.channel.send("Es gibt noch keine Einträge.")
-                embed = discord.Embed(title=f"Bestenliste (Top 10)", color=color)
+                embed = discord.Embed(title=f"Bestenliste (Top 10)", color=self.embed_color)
                 embed.add_field(name="Rank", value=ranks)
                 embed.add_field(name="User", value=users)
                 embed.add_field(name="Länge", value=laengen)
@@ -138,55 +137,55 @@ class Herbot(discord.Client):
                     ranks = ""
                     users = ""
                     laengen = ""
-                    bl = self.__sql.get_ordered_kleinster()[:10]
+                    bl = self.sql.get_ordered_kleinster()[:10]
                     for i in range(len(bl)):
                         ranks += f"#{i+1}\n"
-                        users += f"{bl[i][0]}\n"
-                        laengen += f"{bl[i][1]}cm\n"
+                        users += f"{bl[i][1]}\n"
+                        laengen += f"{bl[i][2]}cm\n"
 
                     if not bl:
                         await message.channel.send("Es gibt noch keine Einträge.")
                         return
-                    embed = discord.Embed(title=f"Kleinster (Top 10)", color=color)
+                    embed = discord.Embed(title=f"Kleinster (Top 10)", color=self.embed_color)
                     embed.add_field(name="Rank", value=ranks)
                     embed.add_field(name="User", value=users)
                     embed.add_field(name="Länge", value=laengen)
                     await message.channel.send(embed=embed)
         elif command == "!addcom":
             if len(args) == 2:
-                if not self.__sql.text_command_exists(args[0]):
-                    self.__sql.add_text_command(args[0], args[1])
+                if not self.sql.text_command_exists(args[0]):
+                    self.sql.add_text_command(args[0], args[1])
                     await message.channel.send(f"Der Command \"{args[0]}\" wurde hinzugefügt.")
                 else:
                     await message.channel.send(f"Den Command \"{args[0]}\" gibt es schon.")
         elif command == "!delcom":
             if len(args) == 1:
-                if self.__sql.text_command_exists(args[0]):
-                    self.__sql.delete_text_command(args[0])
+                if self.sql.text_command_exists(args[0]):
+                    self.sql.delete_text_command(args[0])
                     await message.channel.send(f"Der Command \"{args[0]}\" wurde gelöscht.")
                 else:
                     await message.channel.send(f"Den Command \"{args[0]}\" gibt es nicht.")
         elif command == "!editcom":
             if len(args) == 2:
-                if self.__sql.text_command_exists(args[0]):
-                    self.__sql.edit_text_command(args[0], args[1])
+                if self.sql.text_command_exists(args[0]):
+                    self.sql.edit_text_command(args[0], args[1])
                     await message.channel.send(f"Der Command \"{args[0]}\" wurde bearbeitet.")
                 else:
                     await message.channel.send(f"Den Command \"{args[0]}\" gibt es nicht.")
         elif command == "!help":
             if len(args) == 0:
-                embed = self.__get_command_list_embed(color, 1)
+                embed = self.get_command_list_embed(color, 1)
                 await message.channel.send(embed=embed)
             if len(args) == 1:
                 if args[0].isnumeric():
-                    embed = self.__get_command_list_embed(color, int(args[0]))
+                    embed = self.get_command_list_embed(color, int(args[0]))
                     await message.channel.send(embed=embed)
 
-    def __get_command_list_embed(self, color, page):
-        commands = self.__sql.query("SELECT * FROM commands", ())
+    def get_command_list_embed(self, color, page):
+        commands = self.sql.query("SELECT * FROM commands", ())
         num_pages = math.ceil(len(commands) / 10)
         if (page > num_pages):
-            return self.__get_command_list_embed(color, num_pages)
+            return self.get_command_list_embed(color, num_pages)
         embed = discord.Embed(title=f"Liste mit allen Text Commands (Seite {page}/{num_pages})", color=color)
         all_commands_string = ""
         all_texts_string = ""
@@ -208,36 +207,35 @@ class Herbot(discord.Client):
         
         return embed
                 
-    def __get_stats_embed(self, user, color):
-        display_name = user.display_name
-        embed = discord.Embed(title=f"{display_name} Stats", color=color)
+    def get_stats_embed(self, user, color):
+        embed = discord.Embed(title=f"{user.display_name} Stats", color=color)
         embed.set_thumbnail(url=user.avatar_url_as(size=128))
         embed.add_field(name="Typ", value="Schwanz\nYarak\nSubschwanz\nInsgesamt", inline=True)
         schwaenze = [
-            self.__sql.get_schwanz_laenge(display_name, "schwanz"),
-            self.__sql.get_schwanz_laenge(display_name, "yarak"),
-            self.__sql.get_schwanz_laenge(display_name, "subschwanz")
+            self.sql.get_schwanz_laenge(user, "schwanz"),
+            self.sql.get_schwanz_laenge(user, "yarak"),
+            self.sql.get_schwanz_laenge(user, "subschwanz")
         ]
         insgesamt = 0
         for n, i in enumerate(schwaenze):
             if i != None:
                 insgesamt += schwaenze[n]
 
-        bl = self.__sql.get_ordered_highscores()
+        bl = self.sql.get_ordered_highscores()
         rank = None
         for i in range(len(bl)):
-            if bl[i][0] == display_name:
+            if bl[i][0] == user.id:
                 rank = i+1
                 
-        highscore = self.__sql.get_value_where("highscore", "users", ("display_name", display_name))
+        highscore = self.sql.get_value_where("highscore", "users", ("id", user.id))
 
-        bl_kleinster = self.__sql.get_ordered_kleinster()
+        bl_kleinster = self.sql.get_ordered_kleinster()
         rank_kleinster = None
         for i in range(len(bl_kleinster)):
-            if bl_kleinster[i][0] == display_name:
+            if bl_kleinster[i][0] == user.id:
                 rank_kleinster = i+1
                 
-        kleinster_schwanz = self.__sql.get_value_where("kleinster_schwanz", "users", ("display_name", display_name))
+        kleinster_schwanz = self.sql.get_value_where("kleinster_schwanz", "users", ("id", user.id))
         if highscore is None: highscore = "- "
         if rank is None: rank = "-"
         if rank_kleinster is None: rank_kleinster = "-"
@@ -248,11 +246,11 @@ class Herbot(discord.Client):
         embed.add_field(name="Highscore", value=f"{highscore}cm (#{rank})", inline=True)
         embed.add_field(name="Kleinster", value=f"{kleinster_schwanz}cm (#{rank_kleinster})")
         embed.add_field(name="\u200B", value="\u200B", inline=True)
-        online_time_minutes = self.__sql.get_online_time(display_name)
+        online_time_minutes = self.sql.get_value_where("online_time", "users", ("id", user.id))
         online_time = "{:.2f}".format(online_time_minutes / 60)
         embed.add_field(name="Online Zeit (Voice)", value=f"{online_time} Stunden", inline=False)
 
-        streaming_time_minutes = self.__sql.get_streaming_time(display_name)
+        streaming_time_minutes = self.sql.get_value_where("streaming_time", "users", ("id", user.id))
         streaming_time = "{:.2f}".format(streaming_time_minutes / 60)
         embed.add_field(name="Gestreamte Zeit", value=f"{streaming_time} Stunden")
         return embed
@@ -261,10 +259,11 @@ class Herbot(discord.Client):
     async def count_time(self):
         for i in range(len(self.enabled_members)):
             for member in self.get_guild(self.enabled_members[i][0]).get_channel(self.enabled_members[i][1]).members:
+                self.sql.check_user(member)
                 if member.voice:
                     if member.voice.channel.id in self.ignored_voice_channels: continue
-                    if not self.__sql.is_connected(): self.__sql.reconnect()
-                    self.__sql.add_online_time(member.display_name, 1)
+                    if not self.sql.is_connected(): self.__sql.reconnect()
+                    self.sql.add_online_time(member, 1)
                     if member.voice.self_stream:
-                        self.__sql.add_streaming_time(member.display_name, 1)
+                        self.sql.add_streaming_time(member, 1)
         
--- bot/sql.py
+++ bot/sql.py
@@ -10,7 +10,7 @@ class SQL():
 
     def __create_tables(self):
         self.__cursor.execute("CREATE TABLE IF NOT EXISTS commands (command VARCHAR(32) PRIMARY KEY, text VARCHAR(1024))")
-        self.__cursor.execute("CREATE TABLE IF NOT EXISTS users (display_name VARCHAR(64) PRIMARY KEY, schwanz TINYINT(3) UNSIGNED DEFAULT NULL, yarak TINYINT(3) UNSIGNED DEFAULT NULL, subschwanz TINYINT(3) UNSIGNED DEFAULT NULL, highscore SMALLINT(3) UNSIGNED DEFAULT NULL, online_time INT DEFAULT 0, 187_count INT DEFAULT 0 NOT NULL, streaming_time INT DEFAULT 0, 88_count INT DEFAULT 0, 69_count INT DEFAULT 0, kleinster_schwanz INT DEFAULT NULL, 1337_count int DEFAULT 0, 1337_today BIT(1) DEFAULT 0)")
+        self.__cursor.execute("CREATE TABLE IF NOT EXISTS users (id BIGINT(18) PRIMARY KEY, display_name VARCHAR(64), schwanz TINYINT(3) UNSIGNED DEFAULT NULL, yarak TINYINT(3) UNSIGNED DEFAULT NULL, subschwanz TINYINT(3) UNSIGNED DEFAULT NULL, highscore SMALLINT(3) UNSIGNED DEFAULT NULL, online_time INT DEFAULT 0, 187_count INT DEFAULT 0 NOT NULL, streaming_time INT DEFAULT 0, 88_count INT DEFAULT 0, 69_count INT DEFAULT 0, kleinster_schwanz INT DEFAULT NULL, 1337_count int DEFAULT 0, 1337_today BIT(1) DEFAULT 0)")
         self.__cursor.execute("CREATE TABLE IF NOT EXISTS settings (name VARCHAR(32) PRIMARY KEY, val VARCHAR(512))")
 
     def reconnect(self):
@@ -44,54 +44,68 @@ class SQL():
         else:
             self.execute("INSERT INTO settings (name, val) VALUES (%s, %s)", ('next_date', datetime.today().date()))
  
-    def schwanz_on_cooldown(self, display_name, schwanz_type):
+    def schwanz_on_cooldown(self, user, schwanz_type):
+        laenge = self.get_value_where(schwanz_type, "users", ("id", user.id))
+        if laenge is None: return False
+        else: return True
+        
+        """
         result = self.query("SELECT {} from users WHERE display_name = %s".format(schwanz_type), (display_name,))
         if result[0][0] == None: return False
         else: return True
-
+        """
     def get_ordered_highscores(self):
-        return self.query("SELECT display_name, highscore from users WHERE highscore IS NOT NULL ORDER BY highscore DESC", ())
+        return self.query("SELECT id, display_name, highscore from users WHERE highscore IS NOT NULL ORDER BY highscore DESC", ())
 
     def get_ordered_kleinster(self):
-        return self.query("SELECT display_name, kleinster_schwanz from users WHERE kleinster_schwanz IS NOT NULL ORDER BY kleinster_schwanz ASC", ())
+        return self.query("SELECT id, display_name, kleinster_schwanz from users WHERE kleinster_schwanz IS NOT NULL ORDER BY kleinster_schwanz ASC", ())
 
     def get_value_where(self, column, table, where):
-        return self.query("SELECT {} FROM {} WHERE {} = %s".format(column, table, where[0]), (where[1],))[0][0]
+        val = self.query("SELECT {} FROM {} WHERE {} = %s".format(column, table, where[0]), (where[1],))
+        if val: return val[0][0]
+        else: return None
 
     def update_value_where(self, table, column, value, where):
         self.execute("UPDATE {} SET {} = %s WHERE {} = %s".format(table, column, where[0]), (value, where[1]))
+
+    def check_user(self, user):
+        display_name = self.get_value_where("display_name", "users", ("id", user.id))
+        if display_name:
+            if display_name != user.display_name:
+                self.update_value_where("users", "display_name", user.display_name, ("id", user.id))
+        else:
+            self.add_user(user)
 
     def user_exists(self, display_name):
         result = self.query("SELECT display_name FROM users WHERE display_name = %s", (display_name,))
         if result: return True
         else: return False
 
-    def process_highscore(self, display_name):
-        result = self.query("SELECT schwanz, yarak, subschwanz, highscore, kleinster_schwanz FROM users WHERE display_name = %s", (display_name,))
+    def process_highscore(self, user):
+        result = self.query("SELECT schwanz, yarak, subschwanz, highscore, kleinster_schwanz FROM users WHERE id = %s", (user.id,))
         highscore = result[0][3]
         kleinster_schwanz = result[0][4]
         if None not in result[0][0:3]:
-            print(result[0][0:3])
             schwanz_summe = sum(result[0][0:3])
             if highscore is None or highscore < schwanz_summe:
-                self.execute("UPDATE users SET highscore = %s WHERE display_name = %s", (schwanz_summe, display_name))
+                self.execute("UPDATE users SET highscore = %s WHERE id = %s", (schwanz_summe, user.id))
             if schwanz_summe == 187:
-                self.execute("UPDATE users SET 187_count = 187_count + %s WHERE display_name = %s", (1, display_name))
+                self.execute("UPDATE users SET 187_count = 187_count + %s WHERE id = %s", (1, user.id))
             if schwanz_summe == 69:
-                self.execute("UPDATE users SET 69_count = 69_count + %s WHERE display_name = %s", (1, display_name))
+                self.execute("UPDATE users SET 69_count = 69_count + %s WHERE id = %s", (1, user.id))
             if schwanz_summe == 88:
-                self.execute("UPDATE users SET 88_count = 88_count + %s WHERE display_name = %s", (1, display_name))
+                self.execute("UPDATE users SET 88_count = 88_count + %s WHERE id = %s", (1, user.id))
             if kleinster_schwanz is None or schwanz_summe < kleinster_schwanz:
-                self.execute("UPDATE users SET kleinster_schwanz = %s WHERE display_name = %s", (schwanz_summe, display_name))
+                self.execute("UPDATE users SET kleinster_schwanz = %s WHERE id = %s", (schwanz_summe, user.id))
 
-    def add_user(self, display_name):
-        self.execute("INSERT INTO users (display_name) VALUES (%s)", (display_name,))
+    def add_user(self, user):
+        self.execute("INSERT INTO users (id, display_name) VALUES (%s, %s)", (user.id, user.display_name))
 
-    def update_schwanz_laenge(self, display_name, schwanz_type, laenge):
-        self.update_value_where("users", schwanz_type, laenge, ("display_name", display_name))
+    def update_schwanz_laenge(self, user, schwanz_type, laenge):
+        self.update_value_where("users", schwanz_type, laenge, ("id", user.id))
 
-    def get_schwanz_laenge(self, display_name, schwanz_type):
-        return self.get_value_where(schwanz_type, "users", ("display_name", display_name))
+    def get_schwanz_laenge(self, user, schwanz_type):
+        return self.get_value_where(schwanz_type, "users", ("id", user.id))
 
     def add_text_command(self, command, text):
         self.execute("INSERT INTO commands (command, text) VALUES (%s, %s)", (command, text))
@@ -102,18 +116,12 @@ class SQL():
     def edit_text_command(self, command, text):
         self.update_value_where("commands", "text", text, ("command", command))
 
-    def get_online_time(self, display_name):
-        return self.get_value_where("online_time", "users", ("display_name", display_name))
+    def add_online_time(self, user, minutes):
+        self.execute("UPDATE users SET online_time = online_time + %s WHERE id = %s", (minutes, user.id))
 
-    def get_streaming_time(self, display_name):
-        return self.get_value_where("streaming_time", "users", ("display_name", display_name))
+    def add_streaming_time(self, user, minutes):
+        self.execute("UPDATE users SET streaming_time = streaming_time + %s WHERE id = %s", (minutes, user.id))
 
-    def add_online_time(self, display_name, minutes):
-        self.execute("UPDATE users SET online_time = online_time + %s WHERE display_name = %s", (minutes, display_name))
-
-    def add_streaming_time(self, display_name, minutes):
-        self.execute("UPDATE users SET streaming_time = streaming_time + %s WHERE display_name = %s", (minutes, display_name))
-
-    def add_1337_count(self, display_name, count):
-        self.execute("UPDATE users SET 1337_count = 1337_count + %s WHERE display_name = %s", (count, display_name))
-        self.update_value_where("users", "1337_today", 1, ("display_name", display_name))
+    def add_1337_count(self, user, count):
+        self.execute("UPDATE users SET 1337_count = 1337_count + %s WHERE id = %s", (count, user.id))
+        self.update_value_where("users", "1337_today", 1, ("id", user.id))
